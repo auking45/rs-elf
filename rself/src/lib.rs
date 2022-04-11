df637cd18b702c67be4488e73c6e207ee6f7b3a6
auking45 <auking45@gmail.com>
2022-04-11 22:28:29 +0900

    Define enum for Type
    Signed-off-by: auking45 <auking45@gmail.com>

--- rself/src/lib.rs
+++ rself/src/lib.rs
@@ -26,3 +26,41 @@ impl File {
         Ok((i, Self {}))
     }
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u16)]
+pub enum Type {
+    None = 0x0,
+    Rel = 0x1,
+    Exec = 0x2,
+    Dyn = 0x3,
+    Core = 0x4,
+}
+
+impl Type {
+    pub fn from_u16(x: u16) -> Option<Self> {
+        match x {
+            0 => Some(Self::None),
+            1 => Some(Self::Rel),
+            2 => Some(Self::Exec),
+            3 => Some(Self::Dyn),
+            4 => Some(Self::Core),
+            _ => None,
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn type_to_u16() {
+        assert_eq!(super::Type::Dyn as u16, 0x3);
+    }
+
+    #[test]
+    fn type_from_u16() {
+        assert_eq!(super::Type::from_u16(0x3), Some(super::Type::Dyn));
+        assert_eq!(super::Type::from_u16(0xf00d), None);
+    }
+}
