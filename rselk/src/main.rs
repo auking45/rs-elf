0cef19c008f32f469b86156b71a92b938716f1cf
auking45 <auking45@gmail.com>
2022-04-13 22:40:12 +0900

    Disassembling elf file
    Signed-off-by: auking45 <auking45@gmail.com>

--- rselk/src/main.rs
+++ rselk/src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, error::Error, fs};
-use std::process::Command;
+use std::io::Write;
+use std::process::{Command, Stdio};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input_path = env::args().nth(1).expect("usage: elk FILE");
@@ -11,12 +12,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
     println!("{:#?}", file);
 
-    println!("Executing {:?}...", input_path);
+    println!("Disassembling {:?}...", input_path);
+    let code = &input[0x1000..];
+    let code = &code[..std::cmp::min(0x25, code.len())];
+    ndisasm(code)?;
 
-    let status = Command::new(input_path).status()?;
-    if !status.success() {
-        return Err("process did not exit successfully".into());
-    }
+    Ok(())
+}
+
+fn ndisasm(code: &[u8]) -> Result<(), Box<dyn Error>> {
+    let mut child = Command::new("ndisasm")
+        .arg("-b")
+        .arg("64")
+        .arg("-")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    child.stdin.as_mut().unwrap().write_all(code)?;
+    let output = child.wait_with_output()?;
+    println!("{}", String::from_utf8_lossy(&output.stdout));
 
     Ok(())
 }
