f681dce84dd7888dec5e2e45a4299e3fd752c257
auking45 <auking45@gmail.com>
2022-04-14 22:15:26 +0900

    sample: change permission with mprotect
    Signed-off-by: auking45 <auking45@gmail.com>

--- samples/entry_point.c
+++ samples/entry_point.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-const int cons = 29;
+#include <sys/mman.h>
+#include <errno.h>
+
+const char *instructions = "\x48\x31\xFF\xB8\x3C\x00\x00\x00\x0F\x05";
+const size_t instructions_len = 10;
 
 int main() {
-	printf("main is at %p\n", &main);
-	int numb = 13;
-	printf("numb is at %p\n", &numb);
-	void *aptr = malloc(sizeof(int));
-	printf("cons is at %p\n", &cons);
-	printf("aptr is at %p\n", aptr);
-	free(aptr);
+	printf("        main @ %p\n", &main);
+	printf("instructions @ %p\n", instructions);
+
+	printf("making instructions executable...\n");
+	int ret = mprotect(
+		(void*)instructions,
+		instructions_len,
+		PROT_READ | PROT_EXEC
+	);
+	if (ret != 0) {
+		printf("mprotect failed: error %d", errno);
+		return 1;
+	}
+
+	void (*f)(void) = (void*)instructions;
+	printf("jumping...\n");
+	f();
+	printf("after jump\b");
 }
 
